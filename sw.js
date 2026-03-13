1980cb38d719a885a865eb4a3f87cd4b52082670
derbock203 <43434761+derbock203@users.noreply.github.com>
2026-03-13 21:03:26 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => clients.claim());
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== 'gd2').map(k => caches.delete(k))))); clients.claim(); });
 self.addEventListener('fetch', e => {
   if (e.request.method === 'GET' && !e.request.url.includes('rawg.io'))
-    e.respondWith(caches.open('gd1').then(c => c.match(e.request).then(r => r || (fetch(e.request).then(res => { c.put(e.request, res.clone()); return res })))));
+    e.respondWith(caches.open('gd2').then(c => c.match(e.request).then(r => r || (fetch(e.request).then(res => { c.put(e.request, res.clone()); return res })))));
 });
